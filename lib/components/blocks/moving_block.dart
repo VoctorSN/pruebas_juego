a0f7c2a62de66954062c5ab07d3e476659fdc7ed
AmarinoLama <amarinolama@gmail.com>
2025-04-15 17:40:14 +0200

    change moving_block sprite

--- lib/components/blocks/moving_block.dart
+++ lib/components/blocks/moving_block.dart
@@ -5,8 +5,6 @@ import 'package:flutter_flame/pixel_adventure.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 
-import '../custom_hitbox.dart';
-
 class MovingBlock extends CollisionBlock with HasGameRef<PixelAdventure> {
 
   // Constructor y atributos
@@ -16,23 +14,15 @@ class MovingBlock extends CollisionBlock with HasGameRef<PixelAdventure> {
 
   // Parte de las imágenes
   late final SpriteComponent spriteComponent;
-  Sprite get idleSprite =>
-      Sprite(game.images.fromCache('Traps/Rock Head/Idle.png'));
-  CustomHitbox hitbox = CustomHitbox(
-    offsetX: 5,
-    offsetY: 5,
-    width: 38,
-    height: 38,
-  );
+  Sprite get idleSprite => Sprite(game.images.fromCache('Items/Boxes/Box2/Idle.png'));
 
   // Lógica de movimiento
   late final Player player;
-  late double initialX;
   double pushSpeed = 50.0;
   int pushDirection = 0;
 
   // Lógica de colisión
-  bool isPlayerInline = false;
+  late bool isPlayerInline = false;
   bool isBlockOnLeft = false;
   bool isBlockOnRight = false;
 
@@ -41,21 +31,19 @@ class MovingBlock extends CollisionBlock with HasGameRef<PixelAdventure> {
     await super.onLoad();
     priority = 1;
     debugMode = true;
-
     player = game.player;
-    initialX = position.x;
 
     add(
       RectangleHitbox(
+        position: Vector2(0, 0),
+        size: Vector2(size.x, size.y),
         collisionType: CollisionType.active,
-        position: Vector2(hitbox.offsetX, hitbox.offsetY),
-        size: Vector2(hitbox.width, hitbox.height),
-      )
+      ),
     );
 
     spriteComponent = SpriteComponent(
       sprite: idleSprite,
-      size: size,
+      size: Vector2(size.x, size.y),
       position: Vector2.zero(),
     );
     await add(spriteComponent);
@@ -88,51 +76,16 @@ class MovingBlock extends CollisionBlock with HasGameRef<PixelAdventure> {
     }
   }
 
-  void _movingBlockCollision(CollisionBlock other) {
-    // Verificamos si el otro bloque está completamente debajo del bloque actual
-    final bool isFloor = other.position.y <= position.y + size.y - 1;
-
-    // Si está alineado horizontalmente pero no está por debajo, es colisión lateral
-    if (!isFloor) {
-      if (other.position.x < position.x) {
-        // Tiene un bloque a la izquierda
-        isBlockOnLeft = true;
-      }
-      if (other.position.x > position.x) {
-        // Tiene un bloque a la derecha
-        isBlockOnRight = true;
-      }
-      pushDirection = 0;
-    }
-  }
-
-  void _playerCollision(Player other) {
+  void _collisionPlayer(Player other) {
     final playerMid = other.position.x + other.size.x / 2;
     final blockMid = position.x + size.x / 2;
 
     isPlayerInline = player.y + player.height > position.y && player.y < position.y + size.y;
 
     if (playerMid < blockMid && isPlayerInline && !isBlockOnRight) {
-      // Mueve el bloque a la derecha por lo que deja de tener el bloque a la izquierda
       pushDirection = 1;
       isBlockOnLeft = false;
     } else if (playerMid > blockMid && isPlayerInline && !isBlockOnLeft) {
-      // Mueve el bloque a la izquierda por lo q deja de tener el bloque a la derecha
-      pushDirection = -1;
-      isBlockOnRight = false;
-    }
-  }
-
-  void _collisionPlayer(Player other) {
-    final playerMid = other.position.x + other.size.x / 2;
-    final blockMid = position.x + size.x / 2;
-
-    final bool isPlayerOnBlock = player.y + player.height > position.y && player.y < position.y + size.y;
-
-    if (playerMid < blockMid && isPlayerOnBlock && !isBlockOnRight) {
-      pushDirection = 1;
-      isBlockOnLeft = false;
-    } else if (playerMid > blockMid && isPlayerOnBlock && !isBlockOnLeft) {
       pushDirection = -1;
       isBlockOnRight = false;
     }
