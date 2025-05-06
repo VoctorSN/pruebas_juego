e177159b39cf423093c692b43a77ccfb929a8d56
AmarinoLama <amarinolama@gmail.com>
2025-05-06 16:47:07 +0200

    Refactor spike component and update level-06 spike layout.
    Simplified spike creation logic, adjusted hitbox visuals, and standardized spike size constants in the spike component. Updated the level-06 map to reorganize spike placements and dimensions for improved gameplay consistency.

--- lib/components/game/content/traps/spike.dart
+++ lib/components/game/content/traps/spike.dart
@@ -1,84 +1,79 @@
 import 'package:flame/components.dart';
 import 'package:flame/collisions.dart';
+import 'package:flutter/material.dart';
 import 'package:fruit_collector/components/game/content/levelBasics/player.dart';
 import 'dart:async';
 import '../../../../pixel_adventure.dart';
 
-// TODO: fix the next bugs (FOR THE MOMENT WE USE DEATH AREAS)
-// * right wall => works weird if the wall has 2 spikes, with 1 the player passes without dying
-// * left wall => it could be better, but it works
-// * top wall => doesnt work only in one specific pixel and the correct player direction
-// * bottom wall => the one that works perfectly, coincidence? i dont think so
 class Spike extends PositionComponent
     with HasGameReference<PixelAdventure>, CollisionCallbacks {
-
   // Constructor
   final String wallPosition;
-  Spike({
-    super.position,
-    super.size,
-    this.wallPosition = "BottomWall",
-  });
+
+  Spike({super.position, super.size, this.wallPosition = "BottomWall"});
 
   // Hitbox logic
   late final int cols;
   late final int rows;
+  static const double spikeSize = 16;
 
   @override
   FutureOr<void> onLoad() async {
-
-    //debugMode = true;
-
     await _loadRepeatedSpikes();
 
-    add(RectangleHitbox(
-      size: Vector2(cols*16, 8),
-      anchor: Anchor.topLeft,
-    ));
+    add(
+      RectangleHitbox(anchor: Anchor.topLeft)
+        ..debugMode = true
+        ..debugColor = Colors.red,
+    );
 
     return super.onLoad();
   }
 
   Future<void> _loadRepeatedSpikes() async {
-
     final baseSprite = Sprite(game.images.fromCache('Traps/Spikes/Idle.png'));
+    SpriteComponent spike;
 
-    cols = (size.x / 16).ceil();
-    rows = (size.y / 16).ceil();
+    cols = (size.x / spikeSize).ceil();
+    rows = (size.y / spikeSize).ceil();
 
     for (var row = 0; row < rows; row++) {
       for (var col = 0; col < cols; col++) {
-        final spike = SpriteComponent(
+        spike = SpriteComponent(
           sprite: baseSprite,
-          size: Vector2.all(16),
-          position: Vector2(col * 16, row * 16),
+          size: Vector2.all(spikeSize),
+          position: Vector2(col * spikeSize, row * spikeSize),
         );
+
+        // Rotate the spikes based on the wall position (using π)
+        switch (wallPosition) {
+          case 'TopWall':
+            spike.angle = 3.14159; // 180°
+            spike.position += Vector2.all(spikeSize);
+            break;
+          case 'LeftWall':
+            spike.angle = 1.5708; // 90°
+            spike.position += Vector2(spikeSize, 0);
+            break;
+          case 'RightWall':
+            spike.angle = -1.5708; // -90°
+            spike.position += Vector2(0, spikeSize);
+            break;
+          case 'BottomWall':
+          default:
+            spike.angle = 0;
+            break;
+        }
         add(spike);
       }
-    }
-
-    // Rotate the spikes based on the wall position (using π)
-    switch (wallPosition) {
-      case 'TopWall':
-        angle = 3.14159; // 180°
-        position += Vector2(size.x, size.y);
-        break;
-      case 'LeftWall':
-        angle = 1.5708; // 90°
-        break;
-      case 'RightWall':
-        angle = -1.5708; // -90°
-        position += Vector2(-size.y, size.x);
-        break;
-      case 'BottomWall':
-      default:
-        angle = 0;
-        break;
     }
   }
 
   @override
-  void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other) {
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
     if (other is Player) other.collidedWithEnemy();
     super.onCollisionStart(intersectionPoints, other);
   }
