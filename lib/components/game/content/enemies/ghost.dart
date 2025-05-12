563f2eef30863e71e5742bea5f54ed06d93e9b14
AmarinoLama <amarinolama@gmail.com>
2025-05-12 10:58:06 +0200

    Add ghost sounds and particles

--- lib/components/game/content/enemies/ghost.dart
+++ lib/components/game/content/enemies/ghost.dart
@@ -1,62 +1,80 @@
 import 'dart:async';
 import 'dart:async' as async;
+import 'dart:math' as math;
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/particles.dart';
 import 'package:fruit_collector/pixel_adventure.dart';
+import '../../level/sound_manager.dart';
 import '../../util/utils.dart';
 import '../levelBasics/player.dart';
 
 enum GhostState { appearing, moving, disappearing }
 
-/// TODO refactor the flip of the ghost, now work perfectly but the code is too bad
-/// TODO add the sound of the ghost and particles
+/// TODO: Refactor the flipHorizontal logic
 class Ghost extends SpriteAnimationGroupComponent
     with CollisionCallbacks, HasGameReference<PixelAdventure> {
-
   // Constructor and attributes
   final int spawnIn;
+
   Ghost({super.position, super.size, this.spawnIn = 0});
 
-  // Animations logic
+  // Animation
   late final SpriteAnimation _appearingAnimation;
   late final SpriteAnimation _movingAnimation;
   late final SpriteAnimation _disappearingAnimation;
-  late Sprite trailSprite;
   static final Vector2 spriteSize = Vector2(44, 30);
-  static const stepTime = 0.1;
+  static const double stepTime = 0.1;
 
-  // Movement logic and interactions with player
+  // Particles
+  late final List<Sprite> trailSprites;
+  double _timeSinceLastParticle = 0.0;
+  static const double _particleInterval = 0.2;
+
+  // Movement
   late final Player player = game.player;
   final double speed = 0.5;
-  late final initialPosition = position.clone();
   bool isLookingRight = false;
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
     add(RectangleHitbox(position: Vector2(4, 6), size: Vector2(24, 26)));
-
     _loadAllAnimations();
+    await _loadTrailSprites();
 
     async.Future.delayed(Duration(seconds: spawnIn), _spawn);
-
     return super.onLoad();
   }
 
+  Future<void> _loadTrailSprites() async {
+    final image = game.images.fromCache(
+      'Enemies/Ghost/Gost Particles (48x16).png',
+    );
+    trailSprites = List.generate(
+      4,
+      (i) => Sprite(
+        image,
+        srcPosition: Vector2(16.0 * i, 0),
+        srcSize: Vector2(16, 16),
+      ),
+    );
+  }
+
   void _spawn() async {
+    if (game.isGameSoundsActive) {
+      SoundManager().playAppearGhost(game.gameSoundVolume);
+    }
     current = GhostState.appearing;
     await animationTicker?.completed;
     current = GhostState.moving;
-    // add appearing sound
-    // if (game.isGameSoundsActive) SoundManager().startRockheadAttackingLoop(game.gameSoundVolume);
   }
 
   void respawn() async {
+    if (game.isGameSoundsActive) {
+      SoundManager().playDisappearGhost(game.gameSoundVolume);
+    }
     current = GhostState.disappearing;
     await animationTicker?.completed;
-    // add disappearing sound
-    // if (game.isGameSoundsActive) SoundManager().startRockheadAttackingLoop(game.gameSoundVolume);
-    position = initialPosition;
     async.Future.delayed(const Duration(seconds: 2), _spawn);
   }
 
@@ -70,60 +88,63 @@ class Ghost extends SpriteAnimationGroupComponent
       GhostState.moving: _movingAnimation,
       GhostState.disappearing: _disappearingAnimation,
     };
-
-    trailSprite = Sprite(game.images.fromCache('Enemies/Ghost/Gost Particles (48x16).png'));
   }
 
+  @override
   void update(double dt) {
     super.update(dt);
     if (current == GhostState.moving) {
       _move();
       _flipCorrectly();
-      //_emitTrailParticle();
+      _emitTrailParticle(dt);
     }
   }
 
-  /*void _emitTrailParticle() {
+  void _emitTrailParticle(double dt) {
+    _timeSinceLastParticle += dt;
+    if (_timeSinceLastParticle < _particleInterval) return;
+    _timeSinceLastParticle = 0;
+
     final particle = ParticleSystemComponent(
       position: position.clone() + size / 2,
+      priority: -1,
       particle: Particle.generate(
         count: 1,
-        lifespan: 0.4,
-        generator: (i) => AcceleratedParticle(
-          acceleration: Vector2(0, 5),
-          speed: Vector2.random() * 10 - Vector2.all(5),
-          position: Vector2.zero(),
-          child: SpriteParticle(
-            sprite: trailSprite,
-            size: Vector2(48,16),
-          ),
-        ),
+        lifespan: 0.8,
+        generator:
+            (i) => AcceleratedParticle(
+              acceleration: Vector2(0, 5),
+              speed: Vector2.random() * 10 - Vector2.all(5),
+              position: Vector2(
+                isLookingRight ? -size.x : 0,
+                (math.Random().nextDouble() * 20 - 10),
+              ),
+              child: SpriteParticle(
+                sprite: trailSprites[i % trailSprites.length],
+                size: Vector2(16, 16),
+              ),
+            ),
       ),
     );
+
     parent?.add(particle);
-  }*/
+  }
 
   void _move() {
-
     if (getPlayerXPosition(player) > position.x - 10) {
-      // Ghost goes to the right
-
       position.x += speed;
-      if(!isLookingRight) {
+      if (!isLookingRight) {
         flipHorizontallyAroundCenter();
         isLookingRight = true;
       }
     } else if (getPlayerXPosition(player) < position.x) {
-      // Ghost goes to the left
-
       position.x -= speed;
-      if(isLookingRight) {
+      if (isLookingRight) {
         flipHorizontallyAroundCenter();
         isLookingRight = false;
       }
     }
 
-    // Vertical movement
     if (player.position.y > position.y) {
       position.y += speed;
     } else if (player.position.y < position.y) {
@@ -132,18 +153,12 @@ class Ghost extends SpriteAnimationGroupComponent
   }
 
   void _flipCorrectly() {
-    if (getPlayerXPosition(player) > position.x - 5) {
-      // Ghost goes to the right
-      if(!isLookingRight) {
-        flipHorizontallyAroundCenter();
-        isLookingRight = true;
-      }
-    } else if (getPlayerXPosition(player) < position.x) {
-      // Ghost goes to the left
-      if(isLookingRight) {
-        flipHorizontallyAroundCenter();
-        isLookingRight = false;
-      }
+    if (getPlayerXPosition(player) > position.x - 5 && !isLookingRight) {
+      flipHorizontallyAroundCenter();
+      isLookingRight = true;
+    } else if (getPlayerXPosition(player) < position.x && isLookingRight) {
+      flipHorizontallyAroundCenter();
+      isLookingRight = false;
     }
   }
 
@@ -159,13 +174,15 @@ class Ghost extends SpriteAnimationGroupComponent
   }
 
   @override
-  void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other) {
-    if (other is Player) other.collidedWithEnemy();
-    if (other is Ghost) {
-      // Only the ghost with the lower x-coordinate will respawn
-      if (position.x < other.position.x) {
-        respawn();
-      }
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
+    if (other is Player) {
+      other.collidedWithEnemy();
+    }
+    if (other is Ghost && position.x < other.position.x) {
+      respawn();
     }
     super.onCollisionStart(intersectionPoints, other);
   }
--- lib/components/game/level/sound_manager.dart
+++ lib/components/game/level/sound_manager.dart
@@ -19,6 +19,8 @@ class SoundManager {
   late AudioPool bouncePool;
   late AudioPool smashPool;
   late AudioPool rockheadAttackingPool;
+  late AudioPool appearGhostPool;
+  late AudioPool disappearGhostPool;
 
   bool _initialized = false;
 
@@ -30,33 +32,25 @@ class SoundManager {
     _initialized = true;
 
     collectFruitPool = await AudioPool.createFromAsset(path: 'audio/collect_fruit.wav', maxPlayers: 8);
-
     disappearPool = await AudioPool.createFromAsset(path: 'audio/disappear.wav', maxPlayers: 2);
-
     jumpPool = await AudioPool.createFromAsset(path: 'audio/jump.wav', maxPlayers: 2);
-
     hitPool = await AudioPool.createFromAsset(path: 'audio/hit.wav', maxPlayers: 2);
-
     bouncePool = await AudioPool.createFromAsset(path: 'audio/bounce.wav', maxPlayers: 2);
-
     smashPool = await AudioPool.createFromAsset(path: 'audio/explosion.wav', maxPlayers: 2);
-
     rockheadAttackingPool = await AudioPool.createFromAsset(path: 'audio/rockHeadAttacking.wav', maxPlayers: 2);
+    appearGhostPool = await AudioPool.createFromAsset(path: 'audio/appearGhost.mp3', maxPlayers: 4);
+    disappearGhostPool = await AudioPool.createFromAsset(path: 'audio/disappearGhost.mp3', maxPlayers: 4);
   }
 
   void playCollectFruit(volume) => collectFruitPool.start(volume: volume);
-
   void playHit(volume) => hitPool.start(volume: volume);
-
   void playBounce(volume) => bouncePool.start(volume: volume);
-
   void playDisappear(volume) => disappearPool.start(volume: volume);
-
   void playJump(volume) => jumpPool.start(volume: volume);
-
   void playSmash(volume) => smashPool.start(volume: volume);
-
   void playRockheadAttacking(volume) => rockheadAttackingPool.start(volume: volume);
+  void playAppearGhost(volume) => appearGhostPool.start(volume: volume);
+  void playDisappearGhost(volume) => disappearGhostPool.start(volume: volume);
 
   void startRockheadAttackingLoop(double volume, {Duration interval = const Duration(milliseconds: 500)}) {
     stopRockheadAttackingLoop();
