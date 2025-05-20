ff07a578a72704ceedf40722f735c681b19e189d
VictorSN <victorSN@gmail.com>
2025-05-20 22:55:03 +0200

    feat: add Snail enemy with movement and collision logic; update level-100 to include Snail

---
+++ lib/components/game/content/enemies/snail.dart
@@ -0,0 +1,252 @@
+import 'dart:async';
+import 'dart:async' as async;
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:fruit_collector/components/game/level/sound_manager.dart';
+import 'package:fruit_collector/pixel_adventure.dart';
+
+import '../../content/blocks/collision_block.dart';
+import '../../util/custom_hitbox.dart';
+import '../../util/utils.dart';
+import '../levelBasics/player.dart';
+
+enum SnailState { idle, walk, hit, shellWallHit, shellIdle }
+
+class Snail extends SpriteAnimationGroupComponent with CollisionCallbacks, HasGameReference<PixelAdventure> {
+  // Constructor and attributes
+  final double offNeg;
+  final double offPos;
+  final List<CollisionBlock> collisionBlocks;
+
+  Snail({super.position, super.size, this.offPos = 0, this.offNeg = 0, required this.collisionBlocks});
+
+  static const stepTime = 0.1;
+  static const tileSize = 16;
+  double runSpeed = 60;
+  static const _bounceHeight = 260.0;
+  static final textureSize = Vector2(38, 24);
+  final Random random = Random();
+  bool isOnGround = false;
+  double rangeNeg = 0;
+  double rangePos = 0;
+  double moveDirection = 1;
+  double targetDirection = 1;
+  bool gotStomped = false;
+  late final Player player;
+  double fixedDeltaTime = 1 / 60;
+  double accumulatedTime = 0;
+  int hp = 5;
+  final double _gravity = 9.8;
+  final double _jumpForce = 400;
+  final double _maximunVelocity = 1000;
+  final double _terminalVelocity = 300;
+  int timeToJump = 0;
+
+  late final async.Timer jumpTimer;
+
+  CustomHitbox hitbox = CustomHitbox(offsetX: 10, offsetY: 4, width: 14, height: 28);
+
+  // Animations logic
+  late final SpriteAnimation _idleAnimation;
+  late final SpriteAnimation _walkAnimation;
+  late final SpriteAnimation _hitAnimation;
+  late final SpriteAnimation _shellWallHitAnimation;
+  late final SpriteAnimation _shellIdleAnimation;
+
+  Vector2 velocity = Vector2.zero();
+
+  @override
+  FutureOr<void> onLoad() {
+    player = game.player;
+    add(RectangleHitbox(position: Vector2(4, 6), size: Vector2(24, 26)));
+    _loadAllAnimations();
+    _calculateRange();
+    _startJumpTimer();
+    return super.onLoad();
+  }
+
+  @override
+  void onRemove() {
+    jumpTimer.cancel();
+    super.onRemove();
+  }
+
+  @override
+  void update(double dt) {
+    accumulatedTime += dt;
+    while (accumulatedTime >= fixedDeltaTime) {
+      if (!gotStomped) {
+        _movement(fixedDeltaTime);
+        _checkHorizontalCollisions();
+        _applyGravity(fixedDeltaTime);
+        _checkVerticalCollisions();
+      }
+      accumulatedTime -= fixedDeltaTime;
+    }
+    super.update(dt);
+  }
+
+  void _loadAllAnimations() {
+    _idleAnimation = _spriteAnimation('Idle', 15);
+    _walkAnimation = _spriteAnimation('Walk', 10);
+    _hitAnimation = _spriteAnimation('Hit', 5)..loop = false;
+    _shellWallHitAnimation = _spriteAnimation('Shell Wall Hit', 4)..loop = false;
+    _shellIdleAnimation = _spriteAnimation('Shell Idle', 6);
+
+    animations = {
+      SnailState.idle: _idleAnimation,
+      SnailState.walk: _walkAnimation,
+      SnailState.hit: _hitAnimation,
+      SnailState.shellIdle: _shellIdleAnimation,
+      SnailState.shellWallHit: _shellWallHitAnimation,
+    };
+
+    current = SnailState.idle;
+  }
+
+  void _checkVerticalCollisions() {
+    for (final block in collisionBlocks) {
+      if (block.isPlatform) {
+        if (checkCollisionSnail(this, block)) {
+          if (velocity.y > 0) {
+            isOnGround = true;
+            velocity.y = 0;
+            position.y = block.y - hitbox.height - hitbox.offsetY;
+
+            break;
+          }
+        }
+      } else {
+        if (checkCollisionSnail(this, block)) {
+          if (velocity.y > 0) {
+            velocity.y = 0;
+            position.y = block.y - hitbox.height - hitbox.offsetY;
+            isOnGround = true;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  void _applyGravity(double dt) {
+    velocity.y += _gravity;
+    velocity.y = velocity.y.clamp(-_maximunVelocity, _terminalVelocity);
+    position.y += velocity.y * dt;
+  }
+
+  void _checkHorizontalCollisions() {
+    for (final block in collisionBlocks) {
+      if (!block.isPlatform) {
+        if (checkCollisionSnail(this, block)) {
+          if (velocity.x > 0) {
+            position.x = block.x - width;
+          }
+          if (velocity.x < 0) {
+            position.x = block.x + block.width;
+          }
+          velocity.x = 0;
+          if (current == SnailState.shellIdle) {
+            current = SnailState.shellWallHit;
+              targetDirection = -targetDirection;
+            animationTicker?.completed.then((_) {
+              current = SnailState.shellIdle;
+            });
+          }
+        }
+      }
+    }
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
+    return SpriteAnimation.fromFrameData(
+      game.images.fromCache('Enemies/Snail/$state (38x24).png'),
+      SpriteAnimationData.sequenced(amount: amount, stepTime: stepTime, textureSize: textureSize),
+    );
+  }
+
+  void _startJumpTimer() {
+    timeToJump = random.nextInt(5) + 1;
+    jumpTimer = async.Timer.periodic(Duration(seconds: timeToJump), (_) {
+      _jump();
+      timeToJump = random.nextInt(3) + 1;
+    });
+  }
+
+  void _jump() {
+    if(isSnail()) return;
+    if (game.settings.isSoundEnabled) SoundManager().playJump(game.settings.gameVolume);
+
+    velocity.y = -_jumpForce;
+    isOnGround = false;
+  }
+
+  void _calculateRange() {
+    rangeNeg = position.x - offNeg * tileSize;
+    rangePos = position.x + offPos * tileSize;
+  }
+
+  void _movement(double dt) async {
+    velocity.x = 0;
+
+    double chickenOffset = (scale.x > 0) ? 0 : -width;
+    double playerOffset = (player.scale.x > 0) ? 0 : -player.width;
+
+    if (!isSnail()) {
+      velocity.x = targetDirection * runSpeed;
+    } else if (playerInRange()) {
+      targetDirection = (player.x + playerOffset > position.x + chickenOffset) ? 1 : -1;
+      velocity.x = targetDirection * runSpeed;
+    }
+    moveDirection = lerpDouble(moveDirection, targetDirection, 0.1) ?? 1;
+    position.x += velocity.x * dt;
+  }
+
+  Future<void> attack() async {
+    // isAtacking = true;
+    // current = SnailState.attack;
+    // await animationTicker?.completed;
+    // animationTicker?.reset();
+    // shootProjectile();
+    // current = SnailState.idle;
+    // isAtacking = false;
+    // isDuringCoolDown = true;
+    // Future.delayed(const Duration(milliseconds: attackCouldDown), () {
+    //   isDuringCoolDown = false;
+    // });
+  }
+
+  bool playerInRange() {
+    double playerOffset = (player.scale.x > 0) ? 0 : -player.width;
+
+    return player.x + playerOffset >= rangeNeg &&
+        player.x + playerOffset <= rangePos &&
+        player.y + player.height > position.y &&
+        player.y < position.y + height;
+  }
+
+  void collidedWithPlayer() async {
+    if (player.velocity.y > 0 && player.y + player.height > position.y && isSnail()) {
+      if (game.settings.isSoundEnabled) SoundManager().playBounce(game.settings.gameVolume);
+      current = SnailState.hit;
+      player.velocity.y = -_bounceHeight;
+      await animationTicker?.completed;
+      hp--;
+      if (hp <= 0) {
+        gotStomped = true;
+        removeFromParent();
+      }
+      current = SnailState.shellIdle;
+      runSpeed = 120;
+    } else {
+      player.collidedWithEnemy();
+    }
+  }
+
+  bool isSnail() {
+    return [SnailState.walk, SnailState.idle, SnailState.hit].contains(current);
+  }
+}
--- lib/components/game/util/utils.dart
+++ lib/components/game/util/utils.dart
@@ -4,6 +4,29 @@ import 'package:fruit_collector/components/game/content/enemies/chicken.dart';
 import 'package:fruit_collector/components/game/content/levelBasics/player.dart';
 
 import '../content/enemies/bee.dart';
+import '../content/enemies/snail.dart';
+
+bool checkCollisionSnail(Snail player, block) {
+  final hitbox = player.hitbox;
+
+  final playerX = player.position.x + hitbox.offsetX;
+  final playerY = player.position.y + hitbox.offsetY;
+  final playerWidth = hitbox.width;
+  final playerHeight = hitbox.height;
+
+  final blockX = block.x;
+  final blockY = block.y;
+  final blockWidth = block.width;
+  final blockHeight = block.height;
+
+  final fixedX = player.scale.x < 0 ? playerX - (hitbox.offsetX * 2) - playerWidth : playerX;
+  final fixedY = block.isPlatform ? playerY + playerHeight : playerY;
+
+  return (fixedY < blockY + blockHeight &&
+      playerY + playerHeight > blockY &&
+      fixedX < blockX + blockWidth &&
+      fixedX + playerWidth > blockX);
+}
 
 bool checkCollision(Player player, block) {
   final hitbox = player.hitbox;
